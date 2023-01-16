3c23de41cc5edc766cacc31d637bf446ef2e3504
QBYMKPI <mihmet-saref.akpinar@qbeyond.de>
2023-01-16 13:27:11 +0000

    added basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,17 +1,20 @@
 provider "google" {
   project = var.project_id
-  user_project_override = true
-  billing_project = var.project_id
 }
 
-resource "random_string" "bucket_name" {
+resource "random_string" "folder_name" {
   length           = 8
   special          = false
   upper            = false
+  numeric          = false
 }
 
-module "bucket" {
-  source     = "../.."
-  project_id = var.project_id
-  name       = random_string.bucket_name.result
+data "google_organization" "default" {
+  domain = var.organization_domain
+}
+
+module "folder" {
+  source = "../.."
+  parent = data.google_organization.default.id
+  name   = random_string.folder_name.result
 }
--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -2,3 +2,7 @@ variable "project_id" {
   type = string
   description = "The Project ID where to create the GCS Bucket"
 }
+
+variable "organization_domain" {
+  type = string
+}
