4d67f9338bd69e9812a3292242bfbf37c0f26092
QBYMKPI <mihmet-saref.akpinar@qbeyond.de>
2023-01-18 10:51:58 +0000

    fixed variables

--- examples/iam/variables.tf
+++ examples/iam/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   type = string
-  description = "The Project ID where to create the GCS Bucket"
+  description = "The Project ID where to create the Folder"
 }
 
 variable "organization_domain" {
